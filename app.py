16e48e0dade83b92ab9958f928a772ecba140685
EmmaJimenez1604 <emma_jimenez1601@ciencias.unam.mx>
2023-09-16 14:18:58 -0600

    Ya esta el index, la busqeda por ciudad y ticket. Falta agregar las imagenes de fondo y checar los botones

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def base():
-    return render_template("base.html");
+def index():
+    return render_template("index.html");
+
+@app.route('/search_ticket')
+def searchticket():
+    return render_template('search_ticket.html');
+
+@app.route('/search_city')
+def searchticket():
+    return render_template("search_city.html");
 
 if __name__== "_main_" :
-    app.run(debug = True)
+    app.run(debug = True, port=5000)
