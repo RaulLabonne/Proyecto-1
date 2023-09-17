0f9a7421808b1045dbec5e38dac0239ce61e2ddb
Raul Labonne <raul_labonne27@ciencias.unam.mx>
2023-09-16 19:08:57 -0600

    Variable de entorno en Test

--- src/test/test_search_weather.py
+++ src/test/test_search_weather.py
@@ -1,12 +1,16 @@
 import unittest
 import requests
+from dotenv import load_dotenv
+import os
 
 from src.models.search_weather import getCache
 from src.models.search_weather import searchCache
 from src.models.search_weather import request_iatacode
 from src.models.search_weather import search
 
+load_dotenv()
 
+API_KEY = os.getenv("API_KEY") #La key para hacer llamadas a la api
 class SearchWeather(unittest.TestCase):
 
     def test_read(self):
@@ -19,7 +23,7 @@ class SearchWeather(unittest.TestCase):
         weather = search(city)
         iata = request_iatacode("MEX")  # Da un array de tama\~no 2, donde esta la latitud y longitud
         req = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + iata[0] + '&lon=' + iata[
-            1] + '&appid= f1c784495457a0e077831ac4a2899b22')
+            1] + '&appid=' + API_KEY)
         compare_json = req.json()
         self.assertEqual(weather, compare_json)
 
