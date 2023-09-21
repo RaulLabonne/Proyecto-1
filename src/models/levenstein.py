8bd1463f662d44b5958459a8a91aee36aa0f329e
Alejandro Jacome <jacomedelgadoa@ciencias.unam.mx>
2023-09-20 22:30:44 -0600

    documentado el modelo

--- src/models/levenstein.py
+++ src/models/levenstein.py
@@ -3,6 +3,12 @@ from unicodedata import normalize
 from src.database.readDB import dataCity
 
 def levenstein(str1, str2):
+    """
+    Calculate the Levenshtein distance between two strings.
+    @param str1: the first string
+    @param str2: the second string
+    @return: the Levenshtein distance between the two strings
+    """
     d=dict()
     for i in range(len(str1)+1):
         d[i]=dict()
@@ -15,10 +21,15 @@ def levenstein(str1, str2):
     return d[len(str1)][len(str2)]
 
 def searchLev(code):
-    data = dataCity()
-    line = data[data['IATA']=='zzzz']
+    """
+    Search for a given code in the dataCity dataset using the Levenshtein distance algorithm. 
+    @param code: the string to search
+    @return: the row in the dataset that match the code, or a empty rowf no match is found
+    """
+    data = dataCity() 
+    line = data[data['IATA']=='zzzz'] # line is a empty row 
     lowest = 99
-    row = 0
+    row = 0 # row index
     for i in range(len(data)):
         distance = levenstein(norm(data.loc[i,'IATA']),norm(code))
         distance2 = levenstein(norm(data.loc[i,'cities']),norm(code))
@@ -35,4 +46,10 @@ def searchLev(code):
     return data.iloc[[row]]
 
 def norm(code):
+    """
+    Normalize the given code by applying the NFKD normalization form and converting it to lowercase.
+    @param code: the code to be normalized
+    @return: the normalized code
+    """
+    
     return normalize('NFKD',code).lower()
--- src/models/search_weather.py
+++ src/models/search_weather.py
@@ -1,17 +1,25 @@
 import requests as rq
-import os
-from dotenv import load_dotenv
+from os import getenv
+
 from src.models.levenstein import searchLev
 
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
+API_KEY = getenv("API_KEY")
 
 __cache={}
 
 def getCache(self):
+    """
+    Get the cache value.
+    @return: the cache value
+    """
     return __cache
 
 def search(iata):
+    """
+    Search for weather information based on an IATA code.
+    @param iata: the IATA code of the city to search for.
+    @return: the weather information for the city.
+    """
     row = cityRow(iata)
     key = row.loc[0,'IATA']
     coord = request_iatacode(row)
@@ -21,11 +29,22 @@ def search(iata):
     return weather
 
 def request(coord, key):
+    """
+    Make a request to an API using the given coordinates and store the result in a cache with the given key.
+    @param coord: the coordinates to use for the API request
+    @param key: the key to use for storing the result in the cache
+    @return: the weather data from the API response
+    """
     weather=apiCall(coord)
     __cache[key]=weather
     return weather
 
 def apiCall(coord):
+    """
+    Make an API call to retrieve weather information based on the given coordinates.
+    @param coord: a tuple containing the latitude and longitude coordinates
+    @return: the weather information in JSON format
+    """
     url ='https://api.openweathermap.org/data/2.5/weather?lat='+str(coord[0])+'&lon='+str(coord[1])+'&appid='+API_KEY
     api = rq.get(url)
     if(api.status_code!=200):
@@ -33,15 +52,30 @@ def apiCall(coord):
     return api.json()
 
 def searchCache(iata):
+    """
+    Searches the cache for weather data associated with the given IATA code.
+    @param iata: the IATA code for the location
+    @return: the weather data associated with the given IATA code, or None if not found in the cache.
+    """
     weather=__cache.get(iata)
     return weather
 
 
 def request_iatacode(cityData):
+    """
+    Given city data, extract the latitude and longitude coordinates of the city.
+    @param cityData: the dataFrame containing latitude and longitude information
+    @return: the latitude and longitude coordinates of the city
+    """
     coord=[cityData.loc[0,'latitude'],cityData.loc[0,'longitude']]
     return coord
 
 def cityRow(code):
+    """
+    Given a city code, retrieve the corresponding city data from a search function.
+    @param code: the city code
+    @return: the city data as a pandas DataFrame. If no data is found, return a TypeError.
+    """
     cityData =searchLev(code)
     if(cityData.empty):
         return TypeError
